87dd6a48bfbbdc797df7101faa84a057c2fc1723
EC2 Default User <ec2-user@ip-172-31-30-156.ec2.internal>
2025-02-12 10:33:16 +0000

    changed

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.86.0"
-    }
-  }
-}
 
 # Data source to find the latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
